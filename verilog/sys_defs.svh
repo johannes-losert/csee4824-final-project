26db332c20b39260097b20ccef8f886b35f049a6
johannes <johannes.losert@columbia.edu>
2024-04-04 21:19:00 -0400

    made testbench for btb that compiles

--- verilog/sys_defs.svh
+++ verilog/sys_defs.svh
@@ -392,5 +392,23 @@ typedef struct packed {
     logic issued;
 } RS_ENTRY;
 
+/**
+ * Branch Target Buffer:
+ * 
+ * TODO ADD DESCRIPTION
+ * 
+ */
+
+// entries are written to using the write_entry struct
+typedef struct packed {
+    logic [`XLEN-1:0] branch_pc;
+    logic [`XLEN-1:0] target_pc;
+} BTB_WRITE;
+
+// entries are accessed using index
+typedef struct packed {
+    logic [`XLEN-1:0] target_pc;
+    logic valid;
+} BTB_ENTRY;
 
 `endif // __SYS_DEFS_SVH__
