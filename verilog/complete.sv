8b8022aa93594d02d12e0c1e4b311574db2dd0ba
Zekai Cheng <43442944+zkc2000@users.noreply.github.com>
2024-04-10 18:15:58 -0400

    modified complete.sv

--- verilog/complete.sv
+++ verilog/complete.sv
@@ -2,7 +2,7 @@ module complete(
     input EX_CO_PACKAGE ex_co_reg; //need to add type
     output CO_RE_PACKAGE co_package;
     output logic             regfile_en,  // register write enable
-    output logic [4:0]       regfile_idx, // register write index
+    output PREG              regfile_idx, // register write index
     output logic [`XLEN-1:0] regfile_data // register write data 
 );
 
@@ -13,7 +13,8 @@ module complete(
     // the valid check is also somewhat redundant
     assign regfile_en = ex_co_reg.valid && (ex_co_reg.dest_reg_idx != `ZERO_REG);
 
-    assign regfile_idx = ex_co_reg.dest_reg_idx;
+    assign regfile_idx.reg_num = ex_co_reg.dest_reg_idx;
+    assign regfile_idx.ready = regfile_en;
 
     // Select register writeback data:
     // ALU/MEM result, unless taken branch, in which case we write
