b98ab4647d2fa136f310eeec8f251db441974ac8
Denzel Farmer <denzelgfarmer@gmail.com>
2024-04-15 22:33:22 -0400

    Begin updating retire

--- verilog/retire.sv
+++ verilog/retire.sv
@@ -1,13 +1,17 @@
 module retire(
-    input CO_RE_PACKET      co_package, //need to add type
-    input logic             regfile_en,  // register write enable
-    input logic [4:0]       regfile_idx, // register write index
-    input logic [`XLEN-1:0] regfile_data, // register write data 
+    input CO_RE_PACKET      co_packet, //need to add type
+
+    // input logic             regfile_en,  // register write enable
+    // input logic [4:0]       regfile_idx, // register write index
+    // input logic [`XLEN-1:0] regfile_data, // register write data 
+
     input logic [3:0]       mem2proc_response,
     input logic [$clog2(`ROB_SZ)-1:0] rob_head, //head printer in rob
-    input logic             take_branch,
+    input logic             clear_retire_buffer, // TODO take branch from previous stage or current?
+
     //signals to tell rob to move head
     output logic move_head,
+
     //output of the processor
     output logic [3:0]       pipeline_completed_insts,
     output EXCEPTION_CODE    pipeline_error_status,
@@ -21,16 +25,16 @@ module retire(
 
 
     always_comb begin
-        if (co_package.valid) begin
-            retire_buffer[co_package.rob_index].valid                 = 1;
-            retire_buffer[co_package.rob_index].completed_insts       =  {3'b0, co_package.valid};
-            retire_buffer[co_package.rob_index].NPC                   = co_package.NPC;
-            retire_buffer[co_package.rob_index].error_status = co_package.illegal        ? ILLEGAL_INST :
-                                                                       co_package.halt           ? HALTED_ON_WFI :
+        if (co_packet.valid) begin
+            retire_buffer[co_packet.rob_index].valid                 = 1;
+            retire_buffer[co_packet.rob_index].completed_insts       =  {3'b0, co_packet.valid};
+            retire_buffer[co_packet.rob_index].NPC                   = co_packet.NPC;
+            retire_buffer[co_packet.rob_index].error_status = co_packet.illegal        ? ILLEGAL_INST :
+                                                                       co_packet.halt           ? HALTED_ON_WFI :
                                                                        (mem2proc_response==4'h0) ? LOAD_ACCESS_FAULT : NO_ERROR;
-            retire_buffer[co_package.rob_index].regfile_en    = regfile_en;
-            retire_buffer[co_package.rob_index].regfile_idx   = regfile_idx;
-            retire_buffer[co_package.rob_index].regfile_data  = regfile_data;
+            retire_buffer[co_packet.rob_index].regfile_en    = co_packet.regfile_en;
+            retire_buffer[co_packet.rob_index].regfile_idx   = co_packet.regfile_idx;
+            retire_buffer[co_packet.rob_index].regfile_data  = co_packet.regfile_data;
         end 
         if (retire_buffer[rob_head].valid) begin
             move_head = 1;
