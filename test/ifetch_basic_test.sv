10791c14d1bff2dc3cef376e6d4b0cce7bf1cb6f
johannes <johannes.losert@columbia.edu>
2024-04-18 13:56:08 -0400

    initial implementation of ifetch basic that needs to be tested
    Co-authored-by: RionaWestphal <RionaWestphal@users.noreply.github.com>

---
+++ test/ifetch_basic_test.sv
@@ -0,0 +1,164 @@
+
+`include "verilog/sys_defs.svh"
+`include "test/mem.sv"
+`include "verilog/icache.sv"
+
+module testbench;
+    logic clock, reset;
+
+    // IF MODULE 
+
+    logic if_valid; 
+
+    IF_ID_PACKET if_packet;
+
+    logic [`XLEN-1:0] certain_branch_pc;  // target pc: use if take_branch is TRUE
+    logic certain_branch_req;    // taken-branch signal ONLY FROM EX
+
+    logic [`XLEN-1:0] rob_target_pc;
+    logic rob_target_req;
+
+    logic [`XLEN-1:0] branch_pred_pc;
+    logic branch_pred_req;
+    
+    logic [`XLEN-1:0] proc2Icache_addr;
+
+    logic [63:0]      Icache2proc_data; // data coming back from Instruction memory
+    logic Icache2proc_data_valid;
+
+    logic [3:0] req_debug; 
+    logic [3:0] gnt_debug; 
+    logic [`XLEN-1:0] PC_reg_debug;
+
+    // MEMORY MODULES
+
+    logic [`XLEN-1:0] proc2mem_addr; // address for current command // support for memory model with byte level addressing
+    logic [63:0] proc2mem_data; // address for current command
+    logic[1:0] proc2mem_command; // `BUS_NONE `BUS_LOAD or `BUS_STORE
+
+    logic [3:0]  mem2proc_response; // 0 = can't accept, other=tag of transaction
+    logic [63:0] mem2proc_data;     // data resulting from a load
+    logic [3:0]  mem2proc_tag;       // 0 = no value, other=tag of transaction
+
+
+    ifetch dut(
+        .clock(clock),
+        .reset(reset),
+        
+        .if_valid(if_valid),
+        
+        .certain_branch_pc(certain_branch_pc),
+        .certain_branch_req(certain_branch_req),
+
+        .rob_target_pc(rob_target_pc),
+        .rob_target_req(rob_target_req),
+
+        .branch_pred_pc(branch_pred_pc),
+        .branch_pred_req(branch_pred_req),
+
+        .Icache2proc_data(Icache2proc_data),
+        .Icache2proc_data_valid(Icache2proc_data_valid),
+
+        .if_packet(if_packet),
+        .proc2Icache_addr(proc2Icache_addr),
+        
+        .req_debug(req_debug),
+        .gnt_debug(gnt_debug),
+        .PC_reg_debug(PC_reg_debug)
+    );
+
+    mem mem_0 (
+        .clk(clock),
+
+        .proc2mem_addr(proc2mem_addr),
+        .proc2mem_data(proc2mem_data),
+        // .proc2mem_size(proc2mem_size),
+        .proc2mem_command(proc2mem_command),
+        
+        .mem2proc_response(mem2proc_response),
+        .mem2proc_data(mem2proc_data),
+        .mem2proc_tag(mem2proc_tag)
+    );
+
+    icache icache_0 (
+        .clock(clock),
+        .reset(reset),
+
+        .Imem2proc_response(mem2proc_response),
+        .Imem2proc_data(mem2proc_data),
+        .Imem2proc_tag(mem2proc_tag),
+
+        .proc2Icache_addr(proc2Icache_addr),
+
+        .proc2Imem_command(proc2mem_command),
+        .proc2Imem_addr(proc2mem_addr),
+
+        .Icache_data_out(Icache2proc_data),
+        .Icache_valid_out(Icache2proc_data_valid)
+    );
+
+    always begin       
+        #(`CLOCK_PERIOD/2.0);
+        clock = ~clock;
+    end
+
+    task exit_on_error;
+        begin
+            $display("@@@Failed at time %d", $time);
+            $finish;
+        end
+    endtask
+
+    initial begin  
+        // $monitor(
+        //     "certain_branch_pc = %h, certain_branch_req = %b,\
+        //     rob_target_pc = %h, rob_target_req = %b, \
+        //     branch_pred_pc = %h,\
+        //     branch_pred_req = %b, Icache2proc_data = %h,\
+        //     Icache2proc_data_valid = %b, PC_reg_debug = %h", 
+        //     certain_branch_pc, certain_branch_req,
+        //     rob_target_pc, rob_target_req,
+        //     branch_pred_pc,
+        //     branch_pred_req, 
+        //     Icache2proc_data,
+        //     Icache2proc_data_valid, PC_reg_debug);
+
+        clock = 0;
+        reset = 0;
+        if_valid = 1;
+
+        // Initial Reset
+        reset = 1;
+        @(negedge clock)
+        reset = 0;
+        
+        
+        certain_branch_req = 0;
+        rob_target_req = 0;
+        branch_pred_req = 0;
+        
+        @(posedge if_packet.valid)
+
+        $display("if_packet.valid = %b", if_packet.valid);
+        $display("if_packet.PC = %h", if_packet.PC);
+        assert(if_packet.PC == 32'h0000_0000) else exit_on_error;
+        assert(if_packet.valid == 1) else exit_on_error;
+        
+        @(posedge if_packet.valid)
+
+        $display("if_packet.valid = %b", if_packet.valid);
+        $display("if_packet.PC = %h", if_packet.PC);
+        assert(if_packet.PC == 32'h0000_0004) else exit_on_error;
+        assert(if_packet.valid == 1) else exit_on_error;
+        
+        @(posedge if_packet.valid)
+
+        $display("if_packet.valid = %b", if_packet.valid);
+        $display("if_packet.PC = %h", if_packet.PC);
+        assert(if_packet.PC == 32'h0000_0008) else exit_on_error;
+        assert(if_packet.valid == 1) else exit_on_error;
+        
+        $display("@@@Passed");
+        $finish;
+    end 
+endmodule 
---
+++ verilog/ifetch_basic.sv
@@ -0,0 +1,129 @@
+`include "verilog/sys_defs.svh"
+`include "verilog/psel_gen.sv"
+
+// 1. EX_Branch 2. ROB_Target 3. Branch_Predictor 4. (PC+4)  = 4
+
+// the size of our FIFO instruction buffer
+`define INST_BUF_SIZE 8
+`define INST_INDEX_SIZE $clog2(`INST_BUF_SIZE)
+
+module ifetch (
+    input             clock,          // system clock
+    input             reset,          // system reset
+    
+    //          ***INPUTS***
+
+    input             if_valid,       // only pop the the instruction buffer head if valid
+    
+    // Program Counter Inputs   
+    input [`XLEN-1:0] certain_branch_pc, 
+    input certain_branch_req,
+
+    input [`XLEN-1:0] rob_target_pc,
+    input rob_target_req,
+
+    input [`XLEN-1:0] branch_pred_pc,
+    input branch_pred_req,
+
+    // Icache Request Response
+    input [63:0] Icache2proc_data, // data coming back from Instruction memory
+    input Icache2proc_data_valid, // data coming back from Instruction memory
+
+    //          *** OUTPUTS ***
+
+    // To cache to get inst from memory
+    output [`XLEN-1:0] proc2Icache_addr,
+    // To decode
+    output IF_ID_PACKET if_packet,
+    
+    //          *** DEBUG ***
+    output [3:0] req_debug,
+    output [3:0] gnt_debug,
+    output [`XLEN-1:0] PC_reg_debug,
+    output waiting_on_inst_debug,
+    output [`INST_INDEX_SIZE-1:0] inst_buffer_tail_debug
+);
+
+    logic [`XLEN-1:0] PC_reg; // PC we are currently fetching
+    logic [`XLEN-1:0] n_PC_reg; // PC we are currently fetching
+    
+    logic waiting_on_inst;
+    logic n_waiting_on_inst;  
+    assign waiting_on_inst_debug = waiting_on_inst;
+    
+    logic [3:0] req;
+    assign req = {certain_branch_req, rob_target_req, branch_pred_req, 1'b1};
+    logic [3:0] gnt; 
+
+    IF_ID_PACKET n_if_packet;
+
+    // DEBUG SIGNALS
+    assign req_debug = req;
+    assign gnt_debug = gnt;
+    assign PC_reg_debug = PC_reg;
+    
+    psel_gen #(.WIDTH(4), .REQS(1)) 
+    if_psel (
+        .req(req),       
+        .gnt(gnt),       
+        .gnt_bus(), 
+        .empty()  
+    );
+
+    always_comb begin
+
+        if (if_valid) begin         
+            if (!waiting_on_inst) begin 
+                unique case (gnt)
+                    4'b1000 : n_PC_reg = certain_branch_pc;
+                    4'b0100 : n_PC_reg = rob_target_pc;
+                    4'b0010 : n_PC_reg = branch_pred_pc;
+                    4'b0001 : n_PC_reg = PC_reg + 4;
+                    default : n_PC_reg = 32'hdeadbeef;
+                endcase
+                n_waiting_on_inst = 1;
+
+                n_if_packet.inst = `NOP;
+                n_if_packet.PC = PC_reg;
+                n_if_packet.NPC = PC_reg + 4;
+                n_if_packet.valid = 0;
+            end else if (waiting_on_inst) begin 
+                n_PC_reg = PC_reg;
+                
+                if (Icache2proc_data_valid) begin
+                    // pushing to the tail of inst buffer
+                    n_waiting_on_inst = 0; 
+                    n_if_packet.inst = PC_reg[2] ? Icache2proc_data[63:32] : Icache2proc_data[31:0];
+                    n_if_packet.PC = PC_reg;
+                    n_if_packet.NPC = PC_reg + 4;
+                    n_if_packet.valid = 1;
+                end else if (!Icache2proc_data_valid) begin
+                    n_waiting_on_inst = 1; 
+                    n_if_packet.inst = `NOP;
+                    n_if_packet.PC = PC_reg;
+                    n_if_packet.NPC = PC_reg + 4;
+                    n_if_packet.valid = 0;
+                end
+            end
+        end else begin
+            n_PC_reg = PC_reg;
+            n_waiting_on_inst = waiting_on_inst;
+            n_if_packet = if_packet;
+        end
+    end
+
+    // synopsys sync_set_reset "reset"
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            waiting_on_inst <= 1;
+            PC_reg <= 0;
+        end else begin
+            PC_reg <= n_PC_reg;
+            waiting_on_inst <= n_waiting_on_inst;
+        end
+    end
+
+    // address of the instruction we're fetching (64 bit memory lines)
+    // mem always gives us 8=2^3 bytes, so ignore the last 3 bits
+    assign proc2Icache_addr = {PC_reg[`XLEN-1:2], 2'b0};
+endmodule
