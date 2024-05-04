9dc10835aff4436a20d48b1fc6c7e258d59e0f20
Denzel Farmer <denzelgfarmer@gmail.com>
2024-05-04 07:03:23 -0400

    Change simple branch test program

--- programs/simple_branch.s
+++ programs/simple_branch.s
@@ -3,10 +3,10 @@
     li t1, 1
     li t2, 2
     bne t1,t2, bt1
-    addi x1, x0, 1
-    addi x2, x0, 2
-    addi x3, x0, 3
-    addi x4, x0, 4
+    ori x1, x0, 1
+    ori x2, x0, 2
+    ori x3, x0, 3
+    ori x4, x0, 4
     mul x7,	x2,	x1 #
     add x3, x2, x7
     wfi
